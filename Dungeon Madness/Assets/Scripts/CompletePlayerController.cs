d008593899fea45dd684cf086c9108ef420c1569
zergcow <zergcow@gmail.com>
2018-07-22 10:25:14 -0500

    Added Wisp

--- Dungeon Madness/Assets/Scripts/CompletePlayerController.cs
+++ Dungeon Madness/Assets/Scripts/CompletePlayerController.cs
@@ -7,11 +7,11 @@ using UnityEngine.UI;
 public class CompletePlayerController : MonoBehaviour {
 
 	public float speed;				//Floating point variable to store the player's movement speed.
-	public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
-	public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
+	//public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
+	//public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
 
 	private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
-	private int count;				//Integer to store the number of pickups collected so far.
+	//private int count;				//Integer to store the number of pickups collected so far.
 
 	// Use this for initialization
 	void Start()
@@ -20,13 +20,13 @@ public class CompletePlayerController : MonoBehaviour {
 		rb2d = GetComponent<Rigidbody2D> ();
 
 		//Initialize count to zero.
-		count = 0;
+		//count = 0;
 
 		//Initialze winText to a blank string since we haven't won yet at beginning.
-		winText.text = "";
+		//winText.text = "";
 
 		//Call our SetCountText function which will update the text with the current value for count.
-		SetCountText ();
+		//SetCountText ();
 	}
 
 	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -48,24 +48,25 @@ public class CompletePlayerController : MonoBehaviour {
 	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
 	void OnTriggerEnter2D(Collider2D other) 
 	{
-		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        Debug.Log(other.gameObject.tag);
 		if (other.gameObject.CompareTag ("PickUp")) 
 		{
 			//... then set the other object we just collided with to inactive.
 			other.gameObject.SetActive(false);
 			
 			//Add one to the current value of our count variable.
-			count = count + 1;
+			//count = count + 1;
 			
 			//Update the currently displayed count by calling the SetCountText function.
-			SetCountText ();
+			//SetCountText ();
 		}
 		
 
 	}
 
 	//This function updates the text displaying the number of objects we've collected and displays our victory message if we've collected all of them.
-	void SetCountText()
+	/**void SetCountText()
 	{
 		//Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
 		countText.text = "Count: " + count.ToString ();
@@ -74,5 +75,5 @@ public class CompletePlayerController : MonoBehaviour {
 		if (count >= 12)
 			//... then set the text property of our winText object to "You win!"
 			winText.text = "You win!";
-	}
+	}**/
 }

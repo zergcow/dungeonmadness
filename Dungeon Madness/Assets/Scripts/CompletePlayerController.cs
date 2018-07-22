47e63092da9fbccc2b46ff21c4b6ca865d76a956
zergcow <zergcow@gmail.com>
2018-07-22 10:29:44 -0500

    cleaned a script

--- Dungeon Madness/Assets/Scripts/CompletePlayerController.cs
+++ Dungeon Madness/Assets/Scripts/CompletePlayerController.cs
@@ -7,26 +7,14 @@ using UnityEngine.UI;
 public class CompletePlayerController : MonoBehaviour {
 
 	public float speed;				//Floating point variable to store the player's movement speed.
-	//public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
-	//public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
 
 	private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
-	//private int count;				//Integer to store the number of pickups collected so far.
 
 	// Use this for initialization
 	void Start()
 	{
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
-
-		//Initialize count to zero.
-		//count = 0;
-
-		//Initialze winText to a blank string since we haven't won yet at beginning.
-		//winText.text = "";
-
-		//Call our SetCountText function which will update the text with the current value for count.
-		//SetCountText ();
 	}
 
 	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -54,26 +42,8 @@ public class CompletePlayerController : MonoBehaviour {
 		{
 			//... then set the other object we just collided with to inactive.
 			other.gameObject.SetActive(false);
-			
-			//Add one to the current value of our count variable.
-			//count = count + 1;
-			
-			//Update the currently displayed count by calling the SetCountText function.
-			//SetCountText ();
 		}
 		
 
 	}
-
-	//This function updates the text displaying the number of objects we've collected and displays our victory message if we've collected all of them.
-	/**void SetCountText()
-	{
-		//Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
-		countText.text = "Count: " + count.ToString ();
-
-		//Check if we've collected all 12 pickups. If we have...
-		if (count >= 12)
-			//... then set the text property of our winText object to "You win!"
-			winText.text = "You win!";
-	}**/
 }

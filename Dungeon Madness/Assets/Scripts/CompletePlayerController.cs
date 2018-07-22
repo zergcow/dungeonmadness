2d23a596d4cfe6562f169e01e6d6a3bd32bde286
Dustin Werner <dustin.werner@xsportfitness.com>
2018-07-22 13:03:47 -0500

    Created Floor and Wall Prefabs for future scripted level generation
    Changed Grid Tilemap for testing to use prefabs snapped to the grid
    
    Changed Wisp to move to position instead of by force.

--- Dungeon Madness/Assets/Scripts/CompletePlayerController.cs
+++ Dungeon Madness/Assets/Scripts/CompletePlayerController.cs
@@ -29,8 +29,8 @@ public class CompletePlayerController : MonoBehaviour {
 		//Use the two store floats to create a new Vector2 variable movement.
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 
-		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-		rb2d.AddForce (movement * speed);
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.velocity = movement * speed;
 	}
 
 	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
---
+++ Dungeon Madness/Assets/Scripts/WallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour {
+
+    public Sprite dmgSprite;
+    public int hp = 5;
+    private SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Awake () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+		
+	}
+	
+    public void DamageWall(int loss)
+    {
+        hp -= loss;
+
+        if (hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+            
+    }
+}

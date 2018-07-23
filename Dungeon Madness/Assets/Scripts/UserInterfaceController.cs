16c4d446280bb6d95ff2a6d94cc6fd64ce05dd73
zergcow <zergcow@gmail.com>
2018-07-22 21:59:33 -0500

    dungeon core

---
+++ Dungeon Madness/Assets/Scripts/UserInterfaceController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterfaceController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void ExitGame ()
+    {
+        Application.Quit();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

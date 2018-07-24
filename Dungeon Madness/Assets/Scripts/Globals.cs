8d42e25e9f46c657e909b8507a48c5459ef1a6ce
efrencecht <dustinawerner@gmail.com>
2018-07-24 08:17:14 -0500

    Started 8 direction checks and replacement.
    Currently only replaces previously blank tiles

--- Dungeon Madness/Assets/Scripts/Globals.cs
+++ Dungeon Madness/Assets/Scripts/Globals.cs
@@ -17,4 +17,64 @@ static class Globals {
     public static GameObject[] NWInverse;
     public static GameObject[] SEInverse;
     public static GameObject[] SWInverse;
+    public static GameObject[] GetDirectionName(int offsetX, int offsetY, out string tileName)
+    {
+        tileName = "";
+        if (offsetX == -1)
+        {
+            if (offsetY == 0)
+            {
+                tileName = "westWall";
+                return WWalls;
+            }
+            else if (offsetY == 1)
+            {
+                tileName = "northWestWall";
+                return NWCorner;
+            }
+            else if (offsetX == -1)
+            {
+                tileName = "southWestWall";
+                return SWCorner;
+            }
+        }
+        else if (offsetX == 1)
+        {
+            if (offsetY == 0)
+            {
+                tileName = "eastWall";
+                return EWalls;
+            }
+            else if (offsetY == 1)
+            {
+                tileName = "northEastWall";
+                return NECorner;
+            }
+            else if (offsetY == -1)
+            {
+                tileName = "southEastWall";
+                return SECorner;
+            }
+        }
+        else if (offsetX == 0)
+        {
+            if (offsetY == 0)
+            {
+                tileName = "floor";
+                return Floors;
+            }
+            else if (offsetY == 1)
+            {
+                tileName = "northWall";
+                return NWalls;
+            }
+            else if (offsetY == -1)
+            {
+                tileName = "southWall";
+                return SWalls;
+            }
+        }
+
+        return null;
+    }
 }
--- Dungeon Madness/Assets/Scripts/WallController.cs
+++ Dungeon Madness/Assets/Scripts/WallController.cs
@@ -5,6 +5,7 @@ using UnityEngine.Tilemaps;
 
 public class WallController : MonoBehaviour {
 
+    private Tilemap tileMap;
     public Sprite dmgSprite;
     public int hp = 5;
     private SpriteRenderer spriteRenderer;
@@ -26,37 +27,46 @@ public class WallController : MonoBehaviour {
             var tilePosX = Mathf.RoundToInt(gameObject.transform.position.x);
             var tilePosY = Mathf.RoundToInt(gameObject.transform.position.y);
             var tilePos = new Vector3Int(tilePosX, tilePosY, 0);
-            var tileMap = gameObject.GetComponentInParent<Tilemap>();
+            tileMap = gameObject.GetComponentInParent<Tilemap>();
             var floorTile = ScriptableObject.CreateInstance<Tile>();
             floorTile.gameObject = Globals.Floors[Random.Range(0, Globals.Floors.Length)];
             tileMap.SetTile(tilePos, floorTile);
 
-            var westTileCur = tileMap.GetTile(new Vector3Int(tilePosX - 1, tilePosY, 0));
-            if (westTileCur == null)
+            for (int x = -1; x <= 1; x++)
             {
-
+                for (int y = -1; y <= 1; y++)
+                {
+                    CheckTileInDirection(tilePosX, tilePosY, x, y);
+                }
             }
-            else if (westTileCur.name != "floor")
-            {
-
-            }
-            var eastTileCur = tileMap.GetTile(new Vector3Int(tilePosX + 1, tilePosY, 0));
-            if (eastTileCur == null)
-            {
-
-                var eastTile = ScriptableObject.CreateInstance<Tile>();
-                eastTile.gameObject = Globals.EWalls[Random.Range(0, Globals.EWalls.Length)];
-                tileMap.SetTile(new Vector3Int(tilePosX + 1, tilePosY, 0), eastTile);
-            }
-            else if (eastTileCur.name != "floor")
-            {
-                
-            }
-            var southTileCur = tileMap.GetTile(new Vector3Int(tilePosX, tilePosY - 1, 0));
-
-            var northTileCur = tileMap.GetTile(new Vector3Int(tilePosX, tilePosY + 1, 0));
-
         }
 
     }
+
+    private void CheckTileInDirection(int tilePosX, int tilePosY, int offsetX, int offsetY)
+    {
+        //prevent infinite loop
+        if (offsetX > 1)
+            return;
+        if (offsetY > 1)
+            return;
+        if (offsetX < -1)
+            return;
+        if (offsetY < -1)
+            return;
+
+        var dirTileCur = tileMap.GetTile(new Vector3Int(tilePosX + offsetX, tilePosY + offsetY, 0));
+        if (dirTileCur == null)
+        {
+            string newName = "";
+            GameObject[] dirWalls = Globals.GetDirectionName(offsetX, offsetY, out newName);
+
+            var newTile = ScriptableObject.CreateInstance<Tile>();
+            newTile.gameObject = dirWalls[Random.Range(0, dirWalls.Length)];
+            newTile.name = newName;
+            tileMap.SetTile(new Vector3Int(tilePosX + offsetX, tilePosY + offsetY, 0), newTile);
+        }
+    }
+
+
 }

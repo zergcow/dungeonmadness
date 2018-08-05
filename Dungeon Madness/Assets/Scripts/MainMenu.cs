9f5b0c21dcb071c6a44ffac4644f2ee56b30b605
zergcow <zergcow@gmail.com>
2018-08-04 21:33:28 -0500

    Added a title screen

---
+++ Dungeon Madness/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string newGameScene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadSelectedScene (string sceneName)
+    {
+        newGameScene = sceneName;
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void ExitGame ()
+    {
+        #if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
